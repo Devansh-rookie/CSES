01059f9a81e8858e1d198cbcef58603c824fb8af
Devansh-rookie <devansh151005@gmail.com>
2025-01-19 02:53:59 +0530

    sfgh

--- misc/haybales/haybales.cpp
+++ misc/haybales/haybales.cpp
@@ -12,8 +12,18 @@ void setIO(string name = "") {
 
 int main() {
     setIO("haybales");
+    int n, q;
+    cin >> n >> q;
+    vector<int> arr(n, 0);
 
-    
+    for(int i = 0;i < n;i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    for(int i = 0;i < q;i++){
+        int a, b;
+        cin >> a >> b;
+
+        cout << upper_bound(arr.begin(), arr.end(), b) - lower_bound(arr.begin(), arr.end(),a) << endl;
+    }
 
     return 0;
 }
